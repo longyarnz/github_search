ec7b42952a102865bc62ec9e30e88ae7e7db03f0
Ayodele Olalekan <longyarnz@gmail.com>
2021-11-06 10:27:22 +0100

    chore(main) - import ShouldRender into App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { ShouldRender } from 'should-render';
 import { Pages } from '../components';
 import { Landing } from '../pages';
 import { Wrapper } from './AppStyles'
