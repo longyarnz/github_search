5d7eb1328035984a15e02c5a8926c51860bedc2e
Ayodele Olalekan <longyarnz@gmail.com>
2021-11-07 19:36:34 +0100

    chore(main) - build authentication flow into the app - send requests to obtain tokens from auth service - save access token to session storage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ShouldRender } from 'should-render';
 import { Pages } from '../components';
 import { Landing, Search, Results } from '../pages';
 import { Wrapper } from './AppStyles'
 
 export const App = () => {
-  const [page, setPage] = useState(Pages.LANDING)
+  const [auth, setAuth] = useState<string>(sessionStorage.getItem('INDICINA_AUTH'))
+  const [page, setPage] = useState(auth ? Pages.SEARCH : Pages.LANDING)
+
+  useEffect(() => {
+    sessionStorage.setItem('INDICINA_AUTH', auth)
+    if (!auth) setPage(Pages.LANDING)
+    else setPage(Pages.SEARCH)
+  }, [auth])
 
   return (
     <Wrapper>
       <ShouldRender if={page === Pages.LANDING}>
-        <Landing changePageTo={setPage} />
+        <Landing authorizeUser={setAuth} />
       </ShouldRender>
 
       <ShouldRender if={page === Pages.SEARCH}>
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,20 +1,42 @@
 import React, { FC } from 'react'
-import { GitHubButton, PageInterface, Pages } from '../../components'
+import { LoginWithGitHubButton, useErrorHandler } from '../../components'
 import { Wrapper } from './LandingStyles'
 
-interface Props extends PageInterface {
+interface Props {
+  readonly authorizeUser: React.Dispatch<React.SetStateAction<string>>
 }
 
 export const Landing: FC<Props> = (props) => {
-  const { changePageTo } = props
+  const { authorizeUser } = props
+  const { setError, Banner } = useErrorHandler()
 
-  const login = () => {
-    changePageTo(Pages.SEARCH)
+  const getAccessToken = async (code: string) => {
+    const url = process.env.REACT_APP_AUTH_URL
+
+    const request = await fetch(url, {
+      method: 'POST',
+      body: `{"code": "${code}"}`,
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+
+    const response = await request.json()
+    const accessToken = response?.data?.access_token as string
+    const error = response?.data?.error as string
+    accessToken && authorizeUser(accessToken)
+    error && setError(error)
+  }
+
+  const login = (code: string) => {
+    getAccessToken(code)
   }
 
   return (
     <Wrapper>
-      <GitHubButton text="Login to GitHub" onClick={login} />
+      <Banner>
+        <LoginWithGitHubButton text="Login to GitHub" onClick={login} />
+      </Banner>
     </Wrapper>
   )
 }
