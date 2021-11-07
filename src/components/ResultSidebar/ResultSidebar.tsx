7a945cc33d46f11453f89bde0f22e8f6682abf7a
Ayodele Olalekan <longyarnz@gmail.com>
2021-11-07 20:32:45 +0100

    chore(main) - show active state in sidebar buttons

--- src/components/ResultSidebar/ResultSidebar.tsx
+++ src/components/ResultSidebar/ResultSidebar.tsx
@@ -1,28 +1,32 @@
 import React, { FC } from 'react'
 import { Wrapper } from './ResultSidebarStyles'
-import { ResultButton } from '../'
+import { ResultButton, ResultType } from '../'
 
 interface Props {
   readonly userCount: number
   readonly repoCount: number
+  readonly type: ResultType
+  readonly setType: React.Dispatch<React.SetStateAction<ResultType>>
   readonly isFetchingRepository: boolean
   readonly isFetchingUsers: boolean
 }
 
 export const ResultSidebar: FC<Props> = (props) => {
-  const { userCount, repoCount, isFetchingRepository, isFetchingUsers } = props
+  const { userCount, repoCount, type, setType, isFetchingRepository, isFetchingUsers } = props
 
   return (
     <Wrapper>
       <ResultButton
         text="Repositories"
         isLoading={isFetchingRepository}
+        active={type === ResultType.REPOSITORY}
         count={Number(repoCount).toLocaleString()}
         onClick={() => setType(ResultType.REPOSITORY)}
       />
       <ResultButton
         text="Users"
         isLoading={isFetchingUsers}
+        active={type === ResultType.USER}
         count={Number(userCount).toLocaleString()}
         onClick={() => setType(ResultType.USER)}
       />
