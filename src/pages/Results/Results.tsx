9be5ad82bbbc0468ff40e575ed1a111cfd24497c
Ayodele Olalekan <longyarnz@gmail.com>
2021-11-07 20:06:50 +0100

    chore(main) - implement the search feature in the results panel - invoke custom hook for user search - send a request to fetch user results basedon search query

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,22 +1,56 @@
-import React, { FC } from 'react'
-import { Avatar, GitLogo, ResultPanel, ResultSidebar, Searchbar, PageInterface, Pages } from '../../components'
+import React, {
+  FC,
+  useRef,
+  useState,
+  useLayoutEffect
+} from 'react'
+import {
+  Avatar,
+  GitLogo,
+  ResultPanel,
+  ResultSidebar,
+  Searchbar,
+  PageInterface,
+  Pages,
+  ResultType
+} from '../../components'
 import { Wrapper } from './ResultsStyles'
+import { useGetUsers } from './useGetUsers'
 
 interface Props extends PageInterface {
+  readonly query: string
+  readonly setSearchText?: React.Dispatch<React.SetStateAction<string>>
 }
 
 export const Results: FC<Props> = (props) => {
-  const { changePageTo, logout } = props
+  const { changePageTo, logout, query, setSearchText } = props
+  const formRef = useRef<HTMLFormElement>(null)
+  const { users, userCount, isFetchingUsers } = useGetUsers(query)
+
+  useLayoutEffect(() => {
+    const input = formRef.current?.[0] as HTMLInputElement
+    if (input) input.value = query
+  }, [query])
 
   const goBack = () => {
     changePageTo(Pages.SEARCH)
+  }
+
+  const getSearchText = (text: string) => {
+    sessionStorage.setItem('SEARCH_QUERY', text)
+    setSearchText(text)
   }
 
   return (
     <Wrapper>
       <header>
         <GitLogo />
-        <Searchbar width={380} height={40} />
+        <Searchbar
+          width={380}
+          height={40}
+          form={formRef}
+          getSearchText={getSearchText}
+        />
         <Avatar logout={logout} goBack={goBack} />
       </header>
       <div>
